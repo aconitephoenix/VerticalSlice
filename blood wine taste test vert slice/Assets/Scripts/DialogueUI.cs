d8712756ab7d745a684f184606de8d97538754d9
aconitephoenix <jennythanhvi@gmail.com>
2026-04-14 21:49:33 -0700

    more basic setup stuff....

---
+++ blood wine taste test vert slice/Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _dialogueText;
+    [SerializeField] private TMP_Text _charName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
