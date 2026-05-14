34a9f9b5820982bef9959b1674ba9ba0b21e8631
aconitephoenix <jennythanhvi@gmail.com>
2026-05-14 03:08:46 -0700

    tidying things up

--- blood wine taste test vert slice/Assets/Scripts/DialogueNode.cs
+++ blood wine taste test vert slice/Assets/Scripts/DialogueNode.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
 public enum DialogueType
@@ -10,9 +9,9 @@ public enum DialogueType
 
 public enum Speaker
 {
-    Narrator,
     Player,
-    NPC
+    NPC,
+    Narrator
 }
 
 [System.Serializable]
@@ -36,16 +35,10 @@ public struct Options
 
 public class DialogueNode : ScriptableObject
 {
-    // whether this is a nice, mean, or neutral piece of dialogue
-    //public DialogueType _dialogueType = DialogueType.Neutral;
-
     // lines of dialogue
-    //public string[] _lines;
     public Line[] _lines;
 
     // potential player replies
-    //public string[] _playerReplyOptions;
-
     public Options[] _playerReplyOptions;
 
     // player reply outcomes
